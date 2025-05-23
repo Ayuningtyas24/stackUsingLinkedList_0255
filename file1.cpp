e8b7bf8d60ccf56f13b04836ff55c28533d80f6a
Ayuningtyas24 <adyahseptiani@gmail.com>
2025-05-23 08:26:11 +0700

    memanggil popped value dan menambahkan delete temp

--- file1.cpp
+++ file1.cpp
@@ -27,12 +27,28 @@ public:
         top = NULL; // Initialize the stack with a null top pointer
     }
 
-    // Push operation: Insert an element onto the top of the stack
-    int push(int value)
+    //Push operation: Insert an element onto the top of the stack
+int push(int value)
+{
+    Node *newNode = new Node(); // 1. Allocare memory for the new node
+    newNode->data = value; // 2. assign value
+    newNode->next = top; // 3.Set the next pointer of the new node to the top
+    top = newNode; // 4. Update the top pointer to the new node
+    cout << "Push value: " << value << endl;
+    return value;
+}
+
+//Pop operation: Remove the top most element from the stack
+void pop()
+{
+    if (isEmpty())
     {
-        Node *newNode = new Node(); // 1. Allocare memory for the new node
-        newNode->data = value;      // 2. assign value
-        newNode->next = top;        // 3.Set the next pointer of the new node to the top
-        top = newNode;              // 4. Update the top pointer to the new node
+        cout << "Stack is empty." << endl;
     }
+
+    Node *temp = top; //Create a temporary pointer to the top node
+    top = top->next; //Update the top pointer to the next node
+    cout << "Popped value: " << top->data << endl;
+    delete temp;
+}
 }
