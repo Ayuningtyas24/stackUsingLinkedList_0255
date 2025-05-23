ec11f3132af7a5e8c3df873575786fa5c7f89ae4
Ayuningtyas24 <adyahseptiani@gmail.com>
2025-05-23 08:54:36 +0700

    menambahkan case 4 untuk keluar dari program

--- file1.cpp
+++ file1.cpp
@@ -118,5 +118,8 @@ int main()
             cout << "Stack is empty. No top value." << endl;
         }
         break;
+    case 4:
+        cout << "Exiting program." << endl;
+        break;
     }
 }
